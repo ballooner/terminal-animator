78a35177ae2eba7b13aaebd04a73a3391ba50fc6
Home Desktop-WSL Ubuntu <26182964+ballooner@users.noreply.github.com>
2025-05-29 14:43:51 -0400

    Started working on frameBuffer.c/.h methods

--- includes/frameBuffer.h
+++ includes/frameBuffer.h
@@ -1,14 +1,34 @@
 #ifndef FRAMEBUFFER_H
 #define FRAMEBUFFER_H
 
-struct Buffer
+typedef struct Buffer
 {
     int width;
     int height;
-    char frameData[][];
-};
+    char **frameData;
+} buffer_t;
 
-int initBuffer(int width, int height, struct Buffer *buffer);
-int resizeBuffer(struct Buffer *buffer);
+// Allocate frameData memory
+// return:
+// -2 on failure to allocate memory
+// -1 on invalid height/width
+// 0 on success
+int allocateFrameBuffer(buffer_t *buffer);
+
+// Deallocate all the frameData memory
+// return:
+// -1 if buffer is empty
+// 0 on success
+int freeBuffer(buffer_t *buffer);
+
+// Resize the frameData array and change size of buffer
+// to new height/width
+// return:
+// -1 on bad height/width
+// 0 on success
+int resizeBuffer(int height, int width, buffer_t *buffer);
+
+// Replace a buffer with a new buffer
+int replaceBuffer(const buffer_t *newBuffer, buffer_t *oldBuffer);
 
 #endif
